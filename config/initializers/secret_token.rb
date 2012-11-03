222078a1ed5d7224d76262633f83e919b06ec3fd
Savannah Durbin <savannah.durbin@gmail.com>
2012-11-03 11:53:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matcher::Application.config.secret_token = 'a47eb5fb3a63520bf5518d64991bcd77a144cfc7faf761ec8b100de2548d80985bcd7bbee9abdbe250ce80f0f7cfeff8a4eb09e08cd5a588ae91db98254f8621'
