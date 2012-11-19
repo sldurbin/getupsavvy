2adb2f8386ee671d7f8b8631a803d83ac4c5e06a
Savannah Durbin <savannah.durbin@gmail.com>
2012-11-18 23:04:22 -0500

    Added add picpost page

---
+++ app/controllers/comment_ratings_controller.rb
@@ -0,0 +1,27 @@
+class CommentRatingsController < ApplicationController
+  before_filter :signed_in_user
+
+  def new
+  end
+
+  def create
+    @picture_comment = PictureComment.find_by_id(params[:comment_rating][:picture_comment_id])
+    rating = params[:comment_rating][:rating]
+    current_user.rate_comment!(@picture_comment,rating)
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def destroy
+    @picture_comment = PictureComment.find_by_id(params[:comment_rating][:picture_comment_id])
+    current_user.unrate_comment!(@picture_comment)
+    respond_to do |format|
+      format.html 
+      format.js
+    end
+
+  end
+
+end
